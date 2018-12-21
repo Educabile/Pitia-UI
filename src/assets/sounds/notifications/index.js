03de07b3ceca6f2c8654198cf195974b3c217043
Claudio Cortese <claudio.cortese@outlook.it>
2018-12-21 12:20:41 +0100

    feat: add multiple audio formats

--- src/assets/sounds/notifications/index.js
+++ src/assets/sounds/notifications/index.js
@@ -1,39 +1,147 @@
-import notification from './notification.ogg'
-import notification2 from './notification2.ogg'
-import notification3 from './notification3.ogg'
-import notification4 from './notification4.ogg'
-import notification5 from './notification5.ogg'
-import notification6 from './notification6.ogg'
-import notification7 from './notification7.ogg'
-import notification8 from './notification8.ogg'
-import notification9 from './notification9.ogg'
-import notification10 from './notification10.ogg'
-import notification11 from './notification11.ogg'
-import notification12 from './notification12.ogg'
-import notification13 from './notification13.ogg'
-import notification14 from './notification14.ogg'
-import notification15 from './notification15.ogg'
-import notification16 from './notification16.ogg'
-import notification17 from './notification17.ogg'
-import notification18 from './notification18.ogg'
+import notificationOgg from './notification.ogg'
+import notificationOpus from './notification.opus'
+import notificationWav from './notification.wav'
+import notificationMp3 from './notification.mp3'
+import notification2Ogg from './notification2.ogg'
+import notification2Opus from './notification2.opus'
+import notification2Wav from './notification2.wav'
+import notification2Mp3 from './notification2.mp3'
+import notification3Ogg from './notification3.ogg'
+import notification3Opus from './notification3.opus'
+import notification3Wav from './notification3.wav'
+import notification3Mp3 from './notification3.mp3'
+import notification4Ogg from './notification4.ogg'
+import notification4Opus from './notification4.opus'
+import notification4Wav from './notification4.wav'
+import notification4Mp3 from './notification4.mp3'
+import notification5Ogg from './notification5.ogg'
+import notification5Opus from './notification5.opus'
+import notification5Wav from './notification5.wav'
+import notification5Mp3 from './notification5.mp3'
+import notification6Ogg from './notification6.ogg'
+import notification6Opus from './notification6.opus'
+import notification6Wav from './notification6.wav'
+import notification6Mp3 from './notification6.mp3'
+import notification7Ogg from './notification7.ogg'
+import notification7Opus from './notification7.opus'
+import notification7Wav from './notification7.wav'
+import notification7Mp3 from './notification7.mp3'
+import notification8Ogg from './notification8.ogg'
+import notification8Opus from './notification8.opus'
+import notification8Wav from './notification8.wav'
+import notification8Mp3 from './notification8.mp3'
+import notification9Ogg from './notification9.ogg'
+import notification9Opus from './notification9.opus'
+import notification9Wav from './notification9.wav'
+import notification9Mp3 from './notification9.mp3'
+import notification10Ogg from './notification10.ogg'
+import notification10Opus from './notification10.opus'
+import notification10Wav from './notification10.wav'
+import notification10Mp3 from './notification10.mp3'
+import notification11Ogg from './notification11.ogg'
+import notification11Opus from './notification11.opus'
+import notification11Wav from './notification11.wav'
+import notification11Mp3 from './notification11.mp3'
+import notification12Ogg from './notification12.ogg'
+import notification12Opus from './notification12.opus'
+import notification12Wav from './notification12.wav'
+import notification12Mp3 from './notification12.mp3'
+import notification13Ogg from './notification13.ogg'
+import notification13Opus from './notification13.opus'
+import notification13Wav from './notification13.wav'
+import notification13Mp3 from './notification13.mp3'
+import notification14Ogg from './notification14.ogg'
+import notification14Opus from './notification14.opus'
+import notification14Wav from './notification14.wav'
+import notification14Mp3 from './notification14.mp3'
+import notification15Ogg from './notification15.ogg'
+import notification15Opus from './notification15.opus'
+import notification15Wav from './notification15.wav'
+import notification15Mp3 from './notification15.mp3'
+import notification16Ogg from './notification16.ogg'
+import notification16Opus from './notification16.opus'
+import notification16Wav from './notification16.wav'
+import notification16Mp3 from './notification16.mp3'
+import notification17Ogg from './notification17.ogg'
+import notification17Opus from './notification17.opus'
+import notification17Wav from './notification17.wav'
+import notification17Mp3 from './notification17.mp3'
+import notification18Ogg from './notification18.ogg'
+import notification18Opus from './notification18.opus'
+import notification18Wav from './notification18.wav'
+import notification18Mp3 from './notification18.mp3'
 
 export {
-  notification,
-  notification2,
-  notification3,
-  notification4,
-  notification5,
-  notification6,
-  notification7,
-  notification8,
-  notification9,
-  notification10,
-  notification11,
-  notification12,
-  notification13,
-  notification14,
-  notification15,
-  notification16,
-  notification17,
-  notification18,
+  notificationOgg,
+  notificationOpus,
+  notificationWav,
+  notificationMp3,
+  notification2Ogg,
+  notification2Opus,
+  notification2Wav,
+  notification2Mp3,
+  notification3Ogg,
+  notification3Opus,
+  notification3Wav,
+  notification3Mp3,
+  notification4Ogg,
+  notification4Opus,
+  notification4Wav,
+  notification4Mp3,
+  notification5Ogg,
+  notification5Opus,
+  notification5Wav,
+  notification5Mp3,
+  notification6Ogg,
+  notification6Opus,
+  notification6Wav,
+  notification6Mp3,
+  notification7Ogg,
+  notification7Opus,
+  notification7Wav,
+  notification7Mp3,
+  notification8Ogg,
+  notification8Opus,
+  notification8Wav,
+  notification8Mp3,
+  notification9Ogg,
+  notification9Opus,
+  notification9Wav,
+  notification9Mp3,
+  notification10Ogg,
+  notification10Opus,
+  notification10Wav,
+  notification10Mp3,
+  notification11Ogg,
+  notification11Opus,
+  notification11Wav,
+  notification11Mp3,
+  notification12Ogg,
+  notification12Opus,
+  notification12Wav,
+  notification12Mp3,
+  notification13Ogg,
+  notification13Opus,
+  notification13Wav,
+  notification13Mp3,
+  notification14Ogg,
+  notification14Opus,
+  notification14Wav,
+  notification14Mp3,
+  notification15Ogg,
+  notification15Opus,
+  notification15Wav,
+  notification15Mp3,
+  notification16Ogg,
+  notification16Opus,
+  notification16Wav,
+  notification16Mp3,
+  notification17Ogg,
+  notification17Opus,
+  notification17Wav,
+  notification17Mp3,
+  notification18Ogg,
+  notification18Opus,
+  notification18Wav,
+  notification18Mp3,
 }
